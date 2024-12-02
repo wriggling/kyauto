f633c7e6b3caa517ba66702332f0f0fc87d9ffff
Chex <harveytheson5@gmail.com>
2024-12-02 05:48:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,46 @@ import os
 from keep_alive import keep_alive
 import websocket
 import pyautogui
+from colorama import Fore, init
+import requests
+import random
+import time
 
+init()
 
-def main():
-    try:
-        while True:
-            # Pause for x seconds
-            time.sleep(1.5)
-            pyautogui.typewrite("BigBalls420")
-            pyautogui.press("enter")
-    
-    except KeyboardInterrupt:
-        print("Script terminated by user.")
+token = os.getenv("token")
+channel_id = os.getenv("channel_id")
+messages = ['i agree', 'true', 'real', 'fax dude', 'yeah', 'exactly', 'true af', 'definitely', 'definitely true', 'fax', 'indeed', 'for sure', 'totally', 'could be', 'maybe', 'i disagree', 'probably', 'probably not', 'idk', 'cool', 'not true', 'incorrect', 'definitely not', 'nah', 'wrong', 'lol', 'okay', 'ok', 'k', 'bruh', 'unreal', 'nope', 'no', 'ikr']
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0',
+    'Authorization': token
+}
 
-if __name__ == "__main__":
-    main()
+userinfo = requests.get('https://canary.discordapp.com/api/v9/users/@me', headers=headers).json()
+username = userinfo["username"]
+discriminator = userinfo["discriminator"]
+userid = userinfo["id"]
+
+print(f'''
+{Fore.CYAN}
+
+╦╔═╦ ╦╔═╗╦ ╦╔╦╗╔═╗
+╠╩╗╚╦╝╠═╣║ ║ ║ ║ ║
+╩ ╩ ╩ ╩ ╩╚═╝ ╩ ╚═╝{Fore.RESET}
+------------------------------
+{Fore.GREEN}USER INFO:
+Username: @{username}
+User ID: {userid} {Fore.RESET}
+''')
+
+while True:
+    wait_time = random.randint(70, 200)
+
+    message = random.choice(messages)
+    json_data = {
+        'content': message
+    }
+    r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", headers=headers, json=json_data)
+    print(f'            {Fore.YELLOW}[%] {Fore.YELLOW}[{r.status_code}] {Fore.LIGHTBLACK_EX}[Waiting {str(wait_time)} seconds or {str(wait_time/60)[0:4]} minutes...] {Fore.RESET} Sent message > {message}')
+    time.sleep(wait_time)
 keep_alive()
