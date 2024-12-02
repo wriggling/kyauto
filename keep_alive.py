164fb936fed874038abaefd910fa7bb7ab5e6ee8
Chex <harveytheson5@gmail.com>
2024-12-02 11:30:15 -0600

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -8,7 +8,7 @@ def main():
     return '<meta http-equiv="refresh" content="0; URL=https://phantom.is-a.dev/support"/>'
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="162.220.253.253", port=45554)
 
 def keep_alive():
     server = Thread(target=run)
