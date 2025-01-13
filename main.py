a582881dda077a3f00dc3a18dc769a7ed8ef7034
Chex <harveytheson5@gmail.com>
2025-01-13 05:35:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import time
 
 token = os.getenv("token")
 channel_id = os.getenv("channel_id")
-messages = ['i agree', 'true', 'real', 'fax dude', 'yeah', 'exactly', 'true af', 'definitely', 'definitely true', 'fax', 'indeed', 'for sure', 'totally', 'could be', 'maybe', 'i disagree', 'probably', 'probably not', 'idk', 'cool', 'not true', 'incorrect', 'definitely not', 'nah', 'wrong', 'lol', 'okay', 'ok', 'k', 'bruh', 'unreal', 'nope', 'fs', 'no', 'ikr']
+messages = ['i agree', 'true', 'real', 'fax dude', 'yeah', 'exactly', 'true af', 'definitely', 'definitely true', 'fax', 'indeed', 'for sure', 'totally', 'could be', 'maybe', 'i disagree', 'probably', 'probably not', 'idk', 'cool', 'not true', 'incorrect', 'definitely not', 'nah', 'wrong', 'lol', 'okay', 'ok', 'k', 'bruh', 'unreal', 'nope', 'fs', 'fs bro', 'feels good being 6 figs', 'who tryna go b4b', 'b4b?', 'f4f?', 'ugly asl', 'nigga what', 'nigga', 'brother', 'we doin the opps so bad ts so sad i promise it ain even funny', 'when you see me dont blink or stare', 'muwop got head tapped n he had to shave his hair', 'im bored', 'im bored asl', 'tired', 'im sleepy', 'tired asf', 'horny asl', 'horny', 'clown', 'yo what', 'ignored', 'test', 'finna smoke', 'finna flame sum sh up', 'high asl', 'who got roblox?', 'who got bo6?', 'who got brawl?', 'eating', 'eating some mc donalds rn', 'my house cost 7 figs', 'big 2025', 'no', 'ikr']
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0',
     'Authorization': token
